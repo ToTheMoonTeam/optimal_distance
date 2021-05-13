7054573298e750a2b7b9074e79fc651b576462a5
Vasily Isaev <vasilii.isaev2@kronshtadt.ru>
2021-05-13 15:53:28 +0300

    add example

--- example_find_path.py
+++ example_find_path.py
@@ -8,24 +8,14 @@ lat_1 = 30.23169
 lon_2 = 60.00034
 lat_2 = 30.36574
 
-r = requests.get(f"http://router.project-osrm.org/route/v1/car/{lon_1},{lat_1};{lon_2},{lat_2}?overview=false""")
 
-# Получим пути
-routes = json.loads(r.content)
-# Получим лучший путь
-route_1 = routes.get("routes")[0]
+def get_path_len(lon_1, lat_1, lon_2, lat_2):
+    r = requests.get(f"http://router.project-osrm.org/route/v1/car/{lon_1},{lat_1};{lon_2},{lat_2}?overview=false""")
+    # Получим пути
+    routes = json.loads(r.content)
+    # Получим лучший путь
+    route_1 = routes.get("routes")[0]
+    return route_1["distance"] / 1000
 
-# ответ
-print(route_1)
-# формат ответа:
-"""
-{
-  'legs': 
-    [
-        {'steps': [], 'weight': 1509.9, 'distance': 14707.6, 'summary': '', 'duration': 1509.9}
-    ], 
-  'weight_name': 'routability', 
-  'weight': 1509.9, 
-  'distance': 14707.6, 
-  'duration': 1509.9} # Нас интересует это - длительность поездки
-"""
+
+print(get_path_len(lon_1, lat_1, lon_2, lat_2))
